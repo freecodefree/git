a405e5aa44011382273666fe97ab1f39642b6e1a
U-SHENZHOU\Tree <308515245@qq.com>
2018-08-21 12:12:28 +0800

    cp1

---
+++ cp1.c
@@ -0,0 +1,46 @@
+#include "stdio.h"
+#include "unistd.h"
+#include "fcntl.h"
+
+#define BUFFSIZE 4096
+#define COPYMODE 0644
+
+void oop(char *s1,char *s2);
+
+int main(int ac,char **av){
+	char buff[BUFFSIZE];
+	int sFd,tFd,numRead;
+
+	if(ac!=3){
+		printf("error in usage!");
+		return 1;
+	}
+
+	if((sFd=open(*++av,O_RDONLY))<0){
+		oop("can't open",*av);
+		return 1;
+	}
+
+	if((tFd=creat(*++av,COPYMODE))<0){
+		oop("can't creat",*av);
+		return 1;
+	}
+
+	while((numRead=read(sFd,buff,BUFFSIZE))>0){
+		if(numRead!=write(tFd,buff,BUFFSIZE)){
+			oop("can't write to",*av);
+			return 1;
+		}
+	}
+	close(sFd);
+	close(tFd);
+
+	return 0;
+}
+
+void oop(char *s1,char *s2){
+	perror("Error:");
+	perror(s1);
+	perror(s2);
+	
+}
