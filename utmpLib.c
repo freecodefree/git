b0d2a525c28a2a49c8d6daff3b8b4a3c08e3f942
U-SHENZHOU\Tree <308515245@qq.com>
2018-08-23 11:10:19 +0800

    openUtmp

--- utmpLib.c
+++ utmpLib.c
@@ -10,3 +10,10 @@
 char utmpBuf[NRECS*UTSIZE];
 int numRecs,curRec,fdUtmp=-1;
 
+int utmpOpen(char *filename){
+	if((fdUtmp=open(filename,O_RDONLY))==-1){
+		perror(filename);
+	}
+	numRecs=curRec=0;
+	return fdUtmp;
+}
